cc1f277a38d039724bfbc336fe43801162aa08af
schiltej <janik.schilter@gmail.com>
2021-06-24 20:56:25 +0200

    püsh

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import app
 
+from flask import render_template
+
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/pages/homo')
+def homo():
+    return render_template("homo.html")
