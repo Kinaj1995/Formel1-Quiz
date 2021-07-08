790f3ec27a082bbd3f30a36061b615595daa6011
schiltej <janik.schilter@edu.teko.ch>
2021-07-08 17:14:23 +0200

    templates/home

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,5 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return "Hello, World!"
-
-@app.route('/pages/homo')
-def homo():
-    return render_template("homo.html")
+def home():
+    return render_template("home.html")
